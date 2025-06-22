1b8a14b0ce932ff3ad23cebc0a062ec07521ea0c
Augustin <augustinusmp@gmail.com>
2025-06-22 23:45:13 +0200

    added interactivity

--- index.js
+++ index.js
@@ -1,44 +1,84 @@
 #!/usr/bin/env node
-'use strict'
+"use strict";
 
-import chalk from 'chalk'
-import boxen from 'boxen'
-import clear from 'clear'
-import inquirer from 'inquirer'
-import Enquirer from 'enquirer'
-import open from 'open'
-import terminalImage from 'terminal-image'
+import chalk from "chalk";
+import boxen from "boxen";
+import clear from "clear";
+import inquirer from "inquirer";
+import Enquirer from "enquirer";
+import open from "open";
+import terminalImage from "terminal-image";
 
-clear()
+clear();
 
 const data = {
-    name: chalk.bold.green("@amuyl"),
-    github: chalk.hex('#787878')("https://github.com/augustinmuyl/"),
-    npx: chalk.hex('#787878')("npx amuyl"),
-    email: chalk.hex('#787878')("augustinusmp@gmail.com"),
+  name: chalk.bold.green("@amuyl"),
+  github: chalk.hex("#787878")("https://github.com/augustinmuyl/"),
+  npx: chalk.hex("#787878")("npx amuyl"),
+  email: chalk.hex("#787878")("augustinmuyl.dev@gmail.com"),
 
-    labelGithub: chalk.hex('#9E9E9E').bold("git:"),
-    labelEmail: chalk.hex('#9E9E9E').bold("eml:"),
-    labelCard: chalk.hex('#9E9E9E').bold("npm:"),
-}
+  labelGithub: chalk.hex("#9E9E9E").bold("git:"),
+  labelEmail: chalk.hex("#9E9E9E").bold("eml:"),
+  labelCard: chalk.hex("#9E9E9E").bold("npm:"),
+};
 
 const card = boxen(
-    [
-        `${data.name}`,
-        ``,
-        `${data.labelGithub} ${data.github}`,
-        `${data.labelCard} ${data.npx}`,
-        `${data.labelEmail} ${data.email}`,
-        ``,
-        `${chalk("omnia mea mecom port.")}`,
-    ].join("\n"),
-    {
-        margin: 0,
-        padding: { top: 1, bottom: 1, right: 2, left: 2},
-        borderStyle: "double",
-        borderColor: "white",
-    }
+  [
+    `${data.name}`,
+    ``,
+    `${data.labelGithub} ${data.github}`,
+    `${data.labelCard} ${data.npx}`,
+    `${data.labelEmail} ${data.email}`,
+    ``,
+    `${chalk("omnia mea mecom porto.")}`,
+  ].join("\n"),
+  {
+    margin: 0,
+    padding: { top: 1, bottom: 1, right: 2, left: 2 },
+    borderStyle: "double",
+    borderColor: "white",
+  },
 );
 
-console.log(card)
+const options = {
+  type: "list",
+  name: "actions",
+  message: "select action",
+  choices: [
+    {
+      name: "| resume",
+      value: async () => {
+        console.log("opening resume ...");
+        open("https://www.google.com");
+      },
+    },
+    "- exit",
+  ],
+};
 
+console.log(card);
+
+function main() {
+  inquirer.prompt(options).then(async (answer) => {
+    if (answer.actions == "- exit") {
+      return;
+    } else {
+      console.log("-".repeat(40));
+      await answer.actions();
+      console.log("-".repeat(40));
+
+      Enquirer.prompt({
+        type: "toggle",
+        name: "again",
+        message: "exit?",
+        default: false,
+      }).then((answer) => {
+        if (answer.again == false) {
+          main();
+        }
+      });
+    }
+  });
+}
+
+main();
